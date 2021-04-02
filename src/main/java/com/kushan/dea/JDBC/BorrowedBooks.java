b8cd7377230eb339c090e67a6346c5cd53b1137d
KushanTharaka97 <tharakakushan@gmail.com>
2021-04-02 06:13:53 +0530

    JDBC

---
+++ src/main/java/com/kushan/dea/JDBC/BorrowedBooks.java
@@ -0,0 +1,20 @@
+package com.kushan.dea.JDBC;
+import java.sql.* ;  // for standard JDBC programs
+import java.math.*;
+
+public class BorrowedBooks {
+    public static void dataGet(){
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/sonoo","root","");  
+            //here sonoo is database name, root is username and password  
+            Statement stmt=con.createStatement();  
+            ResultSet rs=stmt.executeQuery("SELECT Lending_Tittle FROM Lending WHERE Lavailable=='0';");  
+            while(rs.next())  
+            System.out.println(rs.getString(0));  
+            con.close();  
+            }catch(Exception e){ System.out.println(e);}  
+    }
+}
+
